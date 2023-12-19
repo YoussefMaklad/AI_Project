d3c3c4802fba8660d5e7cf02ff647d2e8e42c5a1
Mazen Ashraf <mezom1644@gmail.com>
2023-12-19 18:58:30 +0200

    Test

---
+++ graphController.py
@@ -0,0 +1,62 @@
+from graphProject import Graph
+
+edges = [("A", "B"), ("A", "C"), ("B", "D"), ("C", "D"), ("D", "E"), ("E", "G"), ("D", "G")]
+costs = {
+    ("A", "B"): 1,
+    ("A", "C"): 4,
+    ("B", "D"): 2,
+    ("C", "D"): 1,
+    ("D", "E"): 5,
+    ("E", "G"): 2,
+    ("D", "G"): 1
+}
+heuristic = {"A": 5, "B": 15, "C": 1, "D": 20, "E": 0, "G": 1000}
+g = Graph(edges, costs, heuristic, ["G"])
+
+
+def calling_function(edge, cost, heuristic, strategy, start, goal):
+    g = Graph(edge, cost, heuristic, goal)
+    match strategy:
+        case "ucs":
+            visited, fringe = set(), [(0, start, [])]
+            while fringe:
+                ans = g.ucs(fringe, visited)
+                if ans:
+                    break
+            if ans:
+                return ans
+            return None
+        case "bfs":
+            action - 2
+        case "dfs":
+            visited, fringe = set(), list([[start]])
+            while fringe:
+                ans = g.dfs(fringe, visited)
+                if ans:
+                    break
+            if ans:
+                return ans
+            return ans
+        case "greedy":
+            visited, fringe = set(), []
+            fringe.append((g.heuristic_dict[start], start))
+            while fringe:
+                ans = g.greedy(fringe, visited)
+                if ans:
+                    break
+            if ans:
+                return ans
+            return None
+        case "astar":
+            visited, fringe = set(), []
+            fringe.append((g.heuristic_dict[start] + 0, start))
+            while fringe:
+                ans = g.astar(fringe, visited)
+                if ans:
+                    break
+            if ans:
+                return ans
+            return None
+
+
+print(calling_function(edges, costs, heuristic, "dfs", "A", "G"))
--- graphProject.py
+++ graphProject.py
@@ -2,7 +2,6 @@ from collections import deque as queue
 
 
 class Graph:
-
     def __init__(self, edges, costs, heuristic, goals):
         self.graph_dict = {}
         self.heuristic_dict = {}
@@ -27,6 +26,7 @@ class Graph:
             neighbors = self.graph_dict.get(str(node), [])
             new_path = [path + [element] for element in neighbors]
             return new_path
+
         return wrapper
 
     @staticmethod
@@ -39,6 +39,7 @@ class Graph:
             else:
                 new_path = [(self.heuristic_dict[element], [i for i in path] + [element]) for element in neighbours]
             return new_path
+
         return wrapper
 
     @expand_decorator
@@ -62,9 +63,9 @@ class Graph:
     def expand_dfs(self, node, path):
         pass
 
-    def dfs(self, start):
-        visited, fringe = set(), list([[start]])
-        while fringe:
+    def dfs(self, fringe, visited):
+        if fringe:
+            print(fringe)
             path = fringe.pop()
             node = path[-1]
             if self.check_goal(node):
@@ -73,41 +74,15 @@ class Graph:
                 if tuple(node) not in visited:
                     visited.add(tuple(node))
                     fringe.extend(self.expand_dfs(node, path))
-        return None
-
-    def heuristic(self, current_node):
-        if current_node in self.heuristic_dict:
-            return self.heuristic_dict[current_node]
-        if self.check_goal(current_node):
-            heuristic_value = 0
-        else:
-            cumulative_cost = 0
-            node = current_node
-            while node not in self.goals:
-                neighbors = self.graph_dict.get(str(node), [])
-                if not neighbors:
-                    return float('inf')
-                next_node, _ = min(
-                    [(neighbor, self.costs.get((node, neighbor), float('inf')) + self.heuristic(neighbor))
-                     for neighbor in neighbors],
-                    key=lambda x: x[1]
-                )
-                cumulative_cost += self.costs.get((node, next_node), 0)
-                node = next_node
-            heuristic_value = cumulative_cost
-        self.heuristic_dict[current_node] = heuristic_value
-        return heuristic_value
+        return False
 
     @expand_heuristic_decorator
     def expand_greedy(self, node, path):
         pass
 
-    def greedy(self, start):
-        self.heuristic(start)
-        visited, fringe, path = set(), [], []
-        node = start
-        fringe.append((self.heuristic_dict[node], node))
-        while fringe:
+    def greedy(self, fringe, visited):
+        path = []
+        if fringe:
             h, node = min(fringe)
             fringe.remove((h, node))
             path = node
@@ -118,19 +93,15 @@ class Graph:
                 if tuple(node) not in visited:
                     visited.add(tuple(node))
                     fringe.extend(self.expand_greedy(node, path))
-        return None
+        return False
 
     @expand_heuristic_decorator
     def expand_astar(self, node, path):
         pass
 
-    def astar(self, start):
-        self.heuristic(start)
-        visited, fringe, path = set(), [], []
-        node = start
-        fringe.append((self.heuristic_dict[node] + 0, node))
-        while fringe:
-            print(fringe)
+    def astar(self, fringe, visited):
+        path = []
+        if fringe:
             h, node = min(fringe)
             fringe.remove((h, node))
             path = node
@@ -141,14 +112,13 @@ class Graph:
                 if tuple(node) not in visited:
                     visited.add(tuple(node))
                     fringe.extend(self.expand_astar(node, path))
-        return None
+        return False
 
     def path_cost(self, path):
         return sum(self.costs.get((path[i], path[i + 1]), 1) for i in range(len(path) - 1))
 
-    def ucs(self, start):
-        visited, fringe = set(), [(0, start, [])]
-        while fringe:
+    def ucs(self, fringe, visited):
+        if fringe:
             cost, node, path = min(fringe, key=lambda x: x[0])
             fringe.remove((cost, node, path))
             if self.check_goal(node):
@@ -161,24 +131,4 @@ class Graph:
                         new_cost = cost + self.costs.get(edge, 1)
                         new_path = path + [node]
                         fringe.append((new_cost, neighbor, new_path))
-        return None
-
-
-edges = [("A", "B"), ("A", "C"), ("B", "D"), ("C", "D"), ("D", "E"), ("E", "G"), ("D", "G")]
-costs = {
-    ("A", "B"): 1,
-    ("A", "C"): 4,
-    ("B", "D"): 2,
-    ("C", "D"): 1,
-    ("D", "E"): 5,
-    ("E", "G"): 2,
-    ("D", "G"): 1
-}
-heuristic = {"A": 10, "B": 10, "C": 10, "D": 10, "E": 10, "G": 0}
-g = Graph(edges, costs, heuristic, ["G"])
-print(g.heuristic_dict)
-# print(g.bfs("A"))
-# print(g.dfs("A"))
-# print(g.ucs("A"))
-# print(g.greedy("A"))
-# print(g.astar("A"))
+        return False
